328115e670389b275e013b7e5bca0890268df3e8
Wasser <wasserx@hotmail.com>
2012-04-13 18:30:52 +0200

    Refactored children launching

--- gateway.js
+++ gateway.js
@@ -18,8 +18,6 @@
  *     along with Hubiquitus.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-var socketioConnector = require('./lib/client_connectors/socketio_connector.js');
-var boshConnector = require('./lib/client_connectors/bosh_connector.js');
 var parseOptions = require('./lib/options.js').parse_options;
 var fs = require('fs');
 var fork = require('child_process').fork;
@@ -33,94 +31,69 @@ global.log = require('log4js').getLogger(filename); //Use Case: log.info("Info t
  * Starts the gateway instatiating its modules
  */
 function main(){
-    var child = process.argv[2] == 'child' ? true : false;
     var options;
+    var args = process.argv.splice(2);
+    var sioModule = __dirname + '/lib/client_connectors/socketio_connector.js';
+    var boshModule = __dirname + '/lib/client_connectors/bosh_connector.js';
 
-    if(child){
-        options = parseOptions(process.argv.splice(5));
+    try {
+        var argsIt = 0;
 
-        //Start an instance of the socketio server in the port from argv
-        if(process.argv[3] == 'socket.io'){
-            var args = {
-                logLevel : options['global.loglevel'],
-                port : parseInt(process.argv[4]),
-                namespace : options['socket.io.namespace'],
-                discTimeout : options['socket.io.disctimeout'],
-                ridWindow : options['socket.io.ridwindow']
-            };
-            socketioConnector.run(args);
-        }
+        //See if a config file is specified
+        while(argsIt < args.length && !args[argsIt].match(/--conf/))
+            argsIt++;
 
-        //Start an instance of the bosh server in the port from argv
-        else {
-            boshConnector.run(options);
-        }
-    }
-
-    else{
-        //Options are read synchronously
-        try {
-            var args = process.argv.splice(2);
-            var i = 0;
-
-            //See if a config file is specified
-            while(i < args.length && !args[i].match(/--conf/))
-                i++;
-
-            //If specified read it and add it to the list of args with correct formatting
-            if(i < args.length-1 && args[i].match(/--conf/)){
-                var file =  fs.readFileSync(args[i+1], 'utf8');
-                file = file.split('\n');
-                args = args.splice(i+2);
-                for(i = 0; i < file.length; i++){
-                    if(!file[i].match(/ *#.*/) && !file[i].match(/^ *$/))
-                        args = (args.concat(file[i].split(/ *= */)));
-                }
+        //If specified read it and add it to the list of args with correct formatting
+        if(argsIt < args.length-1 && args[argsIt].match(/--conf/)){
+            var file =  fs.readFileSync(args[argsIt+1], 'utf8');
+            file = file.split('\n');
+            args = args.splice(argsIt+2);
+            for(argsIt = 0; argsIt < file.length; argsIt++){
+                if(!file[argsIt].match(/ *#.*/) && !file[argsIt].match(/^ *$/))
+                    args = (args.concat(file[argsIt].split(/ *= */)));
             }
-
-            options = parseOptions(args);
-        }
-        catch (err) {
-            console.error("Error parsing options. Exiting");
-            console.log(err);
-            process.exit(1);
         }
 
-        var children = [];
-        function forkChild(type, port){
-            var child = fork(__dirname + '/gateway.js',
-                [ 'child', type, port ].concat(args));
-            children.push(child);
-
-            //If there is an error, restart the server. never stop it.
-            child.on('exit', function(code, signal){
-                if(signal == 'SIGKILL') return;
-                var idx = children.indexOf(child);
-                if(idx!=-1) children.splice(idx, 1);
-                log.warn(type + ' server on port ' + port + ' stopped working. Restarting');
-                forkChild(type,port);
-            })
-        }
-
-        //Fork Processes for each port
-        var servers = ['socket.io', 'bosh'];
-        for(var i = 0; i < servers.length; i++){
-            var ports = options[servers[i] + '.ports'];
-            for(var j = 0; j < ports.length; j++)
-                forkChild(servers[i], ports[j]);
-        }
-
-        function exitFunction(){
-            log.info('Stopping Server');
-            for(var i = 0; i < children.length; i++)
-                children[i].kill('SIGKILL');
-        }
-
-        //Always clean up in the end
-        process.on('exit', exitFunction);
-        process.on('SIGINT', exitFunction);
-
+        options = parseOptions(args);
+    }catch (err) {
+        console.error("Error parsing options. Exiting");
+        console.log(err);
+        process.exit(1);
     }
+
+    var children = [];
+    var child;
+    //For each port of socket.io start a new process
+    var socketioArgs = {
+        logLevel : options['global.loglevel'],
+        namespace : options['socket.io.namespace'],
+        discTimeout : options['socket.io.disctimeout'],
+        ridWindow : options['socket.io.ridwindow']
+    };
+
+    for(var i = 0; i < options['socket.io.ports'].length; i++){
+        socketioArgs.port = options['socket.io.ports'][i];
+        child = fork(__dirname + '/lib/worker.js');
+        child.send({module: sioModule, args: socketioArgs});
+        children.push(child);
+    }
+
+    //For each port of bosh start a new process
+    for(var i = 0; i < options['bosh.ports'].length; i++){
+        options['bosh.port'] = options['bosh.ports'][i];
+        child = fork(__dirname + '/lib/worker.js');
+        child.send({module: boshModule, args: options});
+        children.push(child);
+    }
+
+    //Set listeners for exiting events and properly kill children
+    var exitEvents = ['exit', 'SIGINT'];
+    for(var i = 0; i < exitEvents.length; i++)
+        process.on(exitEvents[i], function () {
+            log.info("Stopping hNode...");
+            for(var j = 0; j < children.length; j++)
+                children[j].kill();
+        });
 }
 
 main();
