f9565ecb971922a67348d845bed21d9fa92ff983
Wasser <wasserx@hotmail.com>
2012-06-20 12:48:06 +0200

    Support for mongodb in hGetSubscriptions

--- test/hGetSubscriptions.js
+++ test/hGetSubscriptions.js
@@ -25,10 +25,37 @@ describe('hGetSubscriptions', function(){
     var hCommandController = new config.cmdController(config.cmdParams);
     var cmd;
     var status = require('../lib/codes.js').hResultStatus;
+    var chid = config.db.createPk();
+    var chidInactive = config.db.createPk();
+
 
     before(config.beforeFN)
 
     after(config.afterFN)
+
+    before(function(done){
+        config.createChannel(chid, [config.validJID], config.validJID, true, done);
+    })
+
+    //Subscribe to channel
+    before(function(done){
+        config.subscribeToChannel(config.validJID, chid, done);
+    })
+
+    //Create it active
+    before(function(done){
+        config.createChannel(chidInactive, [config.validJID], config.validJID, true, done);
+    })
+
+    //Subscribe to channel
+    before(function(done){
+        config.subscribeToChannel(config.validJID, chidInactive, done);
+    })
+
+    //Make it inactive
+    before(function(done){
+        config.createChannel(chidInactive, [config.validJID], config.validJID, false, done);
+    })
 
     beforeEach(function(){
         cmd= {
@@ -40,23 +67,31 @@ describe('hGetSubscriptions', function(){
         };
     })
 
-    it('should return hResult ok if user doesnt have subscriptions', function(done){
+    it('should return hResult ok with an array as result if user doesnt have subscriptions', function(done){
         cmd.sender = 'dontexist@a';
         hCommandController.execCommand(cmd, null, function(hResult){
-            hResult.should.have.property('cmd', cmd.cmd);
-            hResult.should.have.property('reqid', cmd.reqid);
-            hResult.should.have.property('status', status.OK);
+            hResult.should.have.property('result').and.be.an.instanceof(Array).and.have.lengthOf(0);
+            done();
+        });
+    })
+
+    it('should return hResult ok with an array as result if user has subscriptions', function(done){
+        hCommandController.execCommand(cmd, null, function(hResult){
             hResult.should.have.property('result').and.be.an.instanceof(Array);
             done();
         });
     })
 
-    it('should return hResult ok if user has have subscriptions', function(done){
+    it('should return hResult ok with an array with a chid subscribed', function(done){
         hCommandController.execCommand(cmd, null, function(hResult){
-            hResult.should.have.property('cmd', cmd.cmd);
-            hResult.should.have.property('reqid', cmd.reqid);
-            hResult.should.have.property('status', status.OK);
-            hResult.should.have.property('result').and.be.an.instanceof(Array);
+            hResult.result.should.include(chid);
+            done();
+        });
+    })
+
+    it('should return hResult ok with an array without a chid subscribed if channel is currently inactive', function(done){
+        hCommandController.execCommand(cmd, null, function(hResult){
+            hResult.result.should.not.include(chidInactive);
             done();
         });
     })
